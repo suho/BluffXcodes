9267560427edd367352d417be3c4f1027215e751
Su Ho <hi@suho.dev>
2025-06-16 16:12:26 +0700

    Add automatic opening for all older Xcodes

--- Sources/Actions/XcodeSelection.swift
+++ Sources/Actions/XcodeSelection.swift
@@ -1,18 +1,20 @@
 struct XcodeSelection {
 
-    static func perform() throws -> (selected: Xcode, latest: Xcode) {
-        return try step(title: "Loading Xcode applications...") {
-            let xcodes = Xcode.find().sorted(by: { $0.shortVersion < $1.shortVersion })
-            if xcodes.isEmpty || xcodes.count < 1 {
+    static func loadAll() throws -> [Xcode] {
+        return try step(title: "Loading Xcode applications in background...") {
+            var xcodes: [Xcode] = []
+            let group = DispatchGroup()
+            group.enter()
+            DispatchQueue.global().async {
+                xcodes = Xcode.find().sorted(by: { $0.shortVersion < $1.shortVersion })
+                group.leave()
+            }
+            group.wait()
+            if xcodes.count < 2 {
                 write("Required at least 2 Xcodes into your system", style: .error)
                 throw Error.notFound
             }
-            let selected = picker(
-                title: "Select the old Xcode you want to use!",
-                options: xcodes
-            )
-            write("Selected Xcode: \(selected.shortVersion)", style: .success)
-            return (selected, xcodes.last!)
+            return xcodes
         }
     }
 }
