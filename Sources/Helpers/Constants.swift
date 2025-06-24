de747ff92234474a76ef27db99f6af5495754e83
Su Ho <hi@suho.dev>
2025-06-24 09:04:37 +0700

    Update Xcode build mapping (#13)
    * 🐛 fix: update supported Xcode build numbers
    
    * 🐛 fix: correct supported Xcode mapping

--- Sources/Helpers/Constants.swift
+++ Sources/Helpers/Constants.swift
@@ -1,7 +1,23 @@
+import Foundation
+import Version
+
 enum Constants {
 
     static let applicationsPath = "/Applications"
     static let xcodeBundleID = "com.apple.dt.Xcode"
     static let bundleVersion = "CFBundleVersion"
     static let shortVersion = "CFBundleShortVersionString"
+
+    // Mapping between macOS major versions and their supported Xcode versions.
+    // Older Xcodes will be updated to use the listed build numbers.
+    private static let supportedXcodes: [Int: (version: Version, build: String)] = [
+        26: (version: Version("16.4")!, build: "23792"),
+        15: (version: Version("16.4")!, build: "23792"),
+        14: (version: Version("15.4")!, build: "22622"),
+        13: (version: Version("14.3.1")!, build: "21815")
+    ]
+
+    static var currentSupported: (version: Version, build: String)? {
+        supportedXcodes[ProcessInfo.processInfo.operatingSystemVersion.majorVersion]
+    }
 }
