6d17beb1b136adf01f84e4455de0b68870532cb1
Su Ho <hi@suho.dev>
2025-06-24 08:50:55 +0700

    🐛 fix: correct supported Xcode mapping

--- Sources/Helpers/Constants.swift
+++ Sources/Helpers/Constants.swift
@@ -8,12 +8,13 @@ enum Constants {
     static let bundleVersion = "CFBundleVersion"
     static let shortVersion = "CFBundleShortVersionString"
 
+    // Mapping between macOS major versions and their supported Xcode versions.
+    // Older Xcodes will be updated to use the listed build numbers.
     private static let supportedXcodes: [Int: (version: Version, build: String)] = [
         26: (version: Version("16.4")!, build: "23792"),
-        15: (version: Version("15.4")!, build: "23065"),
-        14: (version: Version("15.1")!, build: "22939"),
-        13: (version: Version("14.3")!, build: "22733"),
-        12: (version: Version("13.4")!, build: "20503")
+        15: (version: Version("16.4")!, build: "23792"),
+        14: (version: Version("15.4")!, build: "22622"),
+        13: (version: Version("14.3.1")!, build: "21815")
     ]
 
     static var currentSupported: (version: Version, build: String)? {
